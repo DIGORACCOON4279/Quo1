f1d79a038b0ad0812e605dd3ef7b5972e87bd2e2
DIGORACCOON4279 <dilema4000@gmail.com>
2025-03-10 00:13:18 -0500

    Updating Academy Slider

--- src/components/Setting/Setting.jsx
+++ src/components/Setting/Setting.jsx
@@ -1,7 +1,7 @@
 import './Setting.css';
 import HeaderProfile from '../HeaderProfile/HeaderProfile';
 import FooterProfile from '../FooterProfile/FooterProfile';
-import LinkProfile from '../LinkProffile/LinkProfile';
+import LinkProfile from '../LinkProfile/LinkProfile';
 
 
 const Setting = () => {
