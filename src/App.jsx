514dac34ae82833289eb3b851a8809dd6e7795bf
DIGORACCOON4279 <dilema4000@gmail.com>
2025-03-12 05:36:22 -0500

    Making SignOut Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 
-import { SignOutModalProvider } from "./context/SignOutModalContext.jsx";
+import { SignOutModalProvider } from "./context/SignOutModalContext";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import LandingPage from "./components/LandingPage/LandingPage";
